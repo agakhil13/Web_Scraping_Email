96f202cfb89ee7153b0a214553d3add9c3f522c9
agakhil13 <aguptaji1310@gmail.com>
2022-04-05 11:41:45 +0530

    First Commit

---
+++ script.py
@@ -0,0 +1,58 @@
+from mimetypes import MimeTypes
+from urllib import response
+import requests
+from bs4 import BeautifulSoup
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText 
+import datetime
+
+now  = datetime.datetime.now()
+
+#Eamil content holder
+content = ''
+
+
+#Extracting stories
+def extract_news(url):
+    print("Extracting News Stories...")
+    cnt = ''
+    cnt += ('<b>Top Stories:</b>\n'+'<br>'+ '-'*50+'<br>')
+    response = requests.get(url)
+    content = response.content
+    soup = BeautifulSoup(content, 'html.parser')
+    for i,tag in enumerate(soup.find_all('td',attrs={'class':'title','valign':''})):
+        cnt += ((str(i+1)+' :: '+tag.text + "\n" + '<br>') if tag.text!='More' else '')
+    return cnt
+
+cnt = extract_news('https://news.ycombinator.com')
+content += cnt
+content += ('<br>------<br>')
+content += ('<br><br>End of Message')
+# print(content)
+
+#sending Eamil
+print('Composing Email...')
+
+SERVER = 'smtp.gmail.com'
+PORT = 587
+FROM = ''
+TO = ''
+PASS = ''
+
+msg = MIMEMultipart()
+msg['Subject'] = "Top NW stories [Automated Email]" + " " +  str(now.day) + '-' + str(now.month) + '-' + str(now.year)
+msg['From'] = FROM
+msg['To'] = TO
+msg.attach(MIMEText(content, 'html'))
+
+print('Initiating Server...')
+
+server = smtplib.SMTP(SERVER,PORT)
+server.set_debuglevel(1)
+server.ehlo()
+server.starttls()
+server.login(FROM, PASS)
+server.sendmail(FROM, TO.split(','), msg.as_string())
+print('Email sent...')
+server.quit()
